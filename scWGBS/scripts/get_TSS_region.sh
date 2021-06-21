eda4b53372129d7c3f0b6c3381d234e9a5b413ec
alvaannett <46521652+alvaannett@users.noreply.github.com>
2021-06-21 08:17:20 +0200

    Update get_TSS_region.sh

--- scWGBS/scripts/get_TSS_region.sh
+++ scWGBS/scripts/get_TSS_region.sh
@@ -1,4 +1,6 @@
 
+#create TSS file from gene bodies file. TSS+-1000 bp
+
 awk 'NR>1 {print $1 " " ($2 - 1000) " " ($2 + 1000)}' files/hg38.gene.bodies.txt > files/hg38.TSS.txt
 sed  -i '1i chr start end' files/hg38.TSS.txt
 
