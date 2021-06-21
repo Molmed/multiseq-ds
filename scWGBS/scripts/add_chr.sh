a6d401cd849148a56522a3f3f3c84aef8d9edf63
alvaannett <46521652+alvaannett@users.noreply.github.com>
2021-06-21 08:15:17 +0200

    Update add_chr.sh

--- scWGBS/scripts/add_chr.sh
+++ scWGBS/scripts/add_chr.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+#Script to add chr to coordinate files 
+
 for f in /crex/proj/uppstore2017165/nobackup/alva/scWGBS/K562_out_GB_hg38/CpG_coordinates_in_regions/*; do
    cp "$f" "$f~" &&    
    gzip -cd "$f~" | sed '2,$s/^/chr/' | gzip > "$f"
